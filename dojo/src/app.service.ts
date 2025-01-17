46154c9892add11332eb25efb79f7333293fea26
Dridr1 <a.driano@outlook.com>
2025-01-17 17:41:58 -0300

    dojo: adicionado módulo de exemplo

--- dojo/src/app.service.ts
+++ dojo/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'ANA JÚLIA, PARE, POR FAVOR, VOCÊ NÃO É O ZECA PAGODINHO!';
   }
 }
